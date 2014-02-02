a87f710bfae426bd1e8484854b144a0448f17691
Dustin Henrich <dustin.henrich@gmail.com>
2014-02-03 06:46:48 +0900

    Working on MouseOut function
    Problem : When the mouse leaves the divs, it does not change back to
    the original paragraph.

--- application.js
+++ application.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 	$('#basecamp').mouseenter(function(){
-		$('#intro').replaceWith("<h1 class="introAfter">Test</h1>");
+		console.log("Mass enter fire")
+		$('#intro').replaceWith("<h1 class='baseCampDesc2'>Basecamp is the project management tool you</br> wish you had on your last project</h1>");
+	});
+	
+	$('basecamp').mouseout(function(){
+		console.log("Mouse has left")
+		$('#baseCampDesc2').replaceWith("#intro")
 	}
-};
+	
+	$('#highrise').mouseenter(function(){
+		console.log("Mass enter fire")
+		$('#intro').replaceWith("<h1 class='highRiseDesc2'>Highrise remembers the important things</br>about people you'd normally forget </h1>");
+	});
+	
+	$('#campfire').mouseenter(function(){
+		console.log("Mass enter fire")
+		$('#intro').replaceWith("<h1 class='campFireDesc2'>From near or far, Campfire helps teams work</br>together over the web in real-time.</h1>");
+	});
+});
