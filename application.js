af966069b750b2d0cd9aaa0b57a1bc43cca5700a
Dustin Henrich <dustin.henrich@gmail.com>
2014-02-04 21:32:35 +0900

    Cannot show second text
    …

--- application.js
+++ application.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
+
+	var basecamp2 = "<h1 class='baseCamp2'>Basecamp is the project management tool you</br> wish you had on your last project</h1>";
+
 	$('#basecamp').mouseenter(function(){
 		$('#intro').hide();
+		$('#baseCamp2').show();
 	});
 
 	$('#basecamp').mouseleave(function(){
