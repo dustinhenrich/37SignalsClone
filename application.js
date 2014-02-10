aa2e93ecff1142763a66d415fa0b361aeb224cc1
Dustin Henrich <dustin.henrich@gmail.com>
2014-02-10 14:27:11 +0900

    Final
    Everything is working great, thanks to the community helping me.
    
    Last change that need to be made are…
    -Shorten the code
    -Change the styling for the logo and the asterisks

--- application.js
+++ application.js
@@ -1,26 +1,32 @@
 $(document).ready(function(){
 	$("#basecamp2").hide();
 	$("#basecamp").mouseenter(function(){
+		$("#intro>h1, #intro>h5").hide();
 		$("#basecamp2").show();
 	}).mouseleave(function() {
 		$("#basecamp2").hide();
+		$("#intro>h1, #intro>h5").show();
 	});
 });
 
 $(document).ready(function(){
  $("#highrise2").hide();
 	$("#highrise").mouseenter(function(){
+		$("#intro>h1, #intro>h5").hide();
 		$("#highrise2").show();
 	}).mouseleave(function() {
 		$("#highrise2").hide();
+		$("#intro>h1, #intro>h5").show();
 	});
 });
 
 $(document).ready(function(){
  $("#campfire2").hide();
 	$("#campfire").mouseenter(function(){
+		$("#intro>h1, #intro>h5").hide();
 		$("#campfire2").show();
 	}).mouseleave(function() {
 		$("#campfire2").hide();
+		$("#intro>h1, #intro>h5").show();
 	});
 });
