51eb47f01abae28117ce104f4a2575d7320bd0ce
Dustin Henrich <dustin.henrich@gmail.com>
2014-02-11 17:02:22 +0900

    Added the arrows and fixed the jitter

--- application.js
+++ application.js
@@ -1,32 +1,38 @@
 $(document).ready(function(){
 	$("#basecamp2").hide();
+	$("#highrise2").hide();
+	$("#campfire2").hide();
+	$("#basecamp>#arrow_left").hide();
+	$("#highrise>#arrow_left").hide();
+	$("#campfire>#arrow_right").hide();
+
 	$("#basecamp").mouseenter(function(){
 		$("#intro>h1, #intro>h5").hide();
 		$("#basecamp2").show();
+		$("#basecamp>#arrow_left").show();
 	}).mouseleave(function() {
 		$("#basecamp2").hide();
 		$("#intro>h1, #intro>h5").show();
+		$("#basecamp>#arrow_left").hide();
 	});
-});
-
-$(document).ready(function(){
- $("#highrise2").hide();
+ 
 	$("#highrise").mouseenter(function(){
 		$("#intro>h1, #intro>h5").hide();
 		$("#highrise2").show();
+		$("#highrise>#arrow_left").show();
 	}).mouseleave(function() {
 		$("#highrise2").hide();
 		$("#intro>h1, #intro>h5").show();
+		$("#highrise>#arrow_left").hide();
 	});
-});
 
-$(document).ready(function(){
- $("#campfire2").hide();
 	$("#campfire").mouseenter(function(){
 		$("#intro>h1, #intro>h5").hide();
 		$("#campfire2").show();
+		$("#campfire>#arrow_right").show();
 	}).mouseleave(function() {
 		$("#campfire2").hide();
 		$("#intro>h1, #intro>h5").show();
+		$("#campfire>#arrow_right").hide();
 	});
 });
